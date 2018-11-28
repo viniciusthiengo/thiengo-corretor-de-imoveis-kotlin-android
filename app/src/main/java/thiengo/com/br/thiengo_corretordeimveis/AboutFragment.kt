d68ecf2fff4a0b5b3b6877650e0b3953b3210756
Vinícius Thiengo <viniciusthiengo@gmail.com>
2018-11-27 23:27:18 -0200

    Initial commit

---
+++ app/src/main/java/thiengo/com/br/thiengo_corretordeimveis/AboutFragment.kt
@@ -0,0 +1,172 @@
+package thiengo.com.br.thiengo_corretordeimveis
+
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import mehdi.sakout.aboutpage.AboutPage
+import mehdi.sakout.aboutpage.Element
+
+
+class AboutFragment : Fragment() {
+
+    companion object {
+        const val WHATS_APP_NUMBER = "27999887766"
+        const val LINKED_IN_ID = "vinícius-thiengo-5179b180"
+    }
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+
+        val aboutPage = AboutPage(activity)
+            .setImage(R.drawable.thiengo_corretor)
+            .setDescription( getString(R.string.about_description) )
+            .addItem(
+                getItemGroup( R.string.about_contact_group_title )
+            )
+            .addItem( getItemPhone() )
+            .addItem( getItemWhatsApp() )
+            .addEmail(
+                "thiengocalopsita@gmail.com",
+                getString(R.string.about_label_email)
+            )
+            .addWebsite(
+                "https://www.thiengo.com.br",
+                getString(R.string.about_label_website)
+            )
+            .addItem(
+                getItemGroup( R.string.about_work_group_title )
+            )
+            .addYoutube(
+                "thiengoCalopsita",
+                getString(R.string.about_label_youtube)
+            )
+            .addFacebook(
+                "thiengoCalopsita",
+                getString(R.string.about_label_facebook)
+            )
+            .addTwitter(
+                "thiengoCalops",
+                getString(R.string.about_label_twitter)
+            )
+            .addItem( getItemLinkedIn() )
+            .create()
+
+        /*
+         * Colocando o posicionamento do texto de descrição à
+         * esquerda, sendo que o padrão é no centro.
+         * */
+        val description = aboutPage.findViewById<TextView>( R.id.description )
+        description.gravity = Gravity.START
+
+        /*
+         * Acessando o ImageView principal da AboutPage API
+         * para remover a margem de topo e colocar uma margem
+         * de 16dp no fundo.
+         * */
+        val image = aboutPage.findViewById<ImageView>( R.id.image )
+        val layoutParams = image.layoutParams as LinearLayout.LayoutParams
+        layoutParams.topMargin = 0
+        layoutParams.bottomMargin = (16 * activity!!.resources.getDisplayMetrics().density).toInt() /* Colocando em DP. */
+
+        return aboutPage
+    }
+
+    private fun getItemGroup( titleId: Int )
+        = Element()
+            .setTitle( getString( titleId ) )
+            .setGravity( Gravity.CENTER )
+
+    private fun getItemPhone()
+        = Element()
+            .setTitle( getString(R.string.about_phone_title) )
+            .setIconTint( R.color.colorPhoneLogo )
+            .setGravity( Gravity.START )
+            .setIconDrawable( R.drawable.ic_phone_in_talk_black_24dp )
+            .setOnClickListener {
+                (activity as MainActivity).whatsAppHelp()
+            }
+
+    private fun getItemWhatsApp()
+        = Element()
+            .setTitle( getString(R.string.about_whats_app_title) )
+            .setIconTint( R.color.colorWhatsAppLogo )
+            .setGravity( Gravity.START )
+            .setIconDrawable( R.drawable.ic_whatsapp_black_24dp )
+            .setOnClickListener {
+                val intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse( "smsto:27999887766" )
+                )
+                intent.setPackage( "com.whatsapp" )
+
+                /*
+                 * Caso o aplicativo no LinkedIn não esteja
+                 * instalado, abre o perfil via navegador
+                 * mobile.
+                 * */
+                if( intent.resolveActivity( activity!!.packageManager ) != null ){
+                    activity!!.startActivity( intent )
+                }
+                else{
+                    Toast
+                        .makeText(
+                            activity,
+                            getString(R.string.whatsapp_needed_info),
+                            Toast.LENGTH_SHORT
+                        )
+                        .show()
+                }
+            }
+
+    private fun getItemLinkedIn()
+        = Element()
+            .setTitle( getString(R.string.about_linked_in_title) )
+            .setIconTint( R.color.colorLinkedInLogo )
+            .setGravity( Gravity.START )
+            .setIconDrawable( R.drawable.ic_linkedin_box_black_24dp )
+            .setOnClickListener {
+                var intent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse( "linkedin://profile/$LINKED_IN_ID" )
+                )
+                intent.setPackage( "com.linkedin.android" )
+
+                /*
+                 * Caso o aplicativo no LinkedIn não esteja
+                 * instalado, abre o perfil via navegador
+                 * mobile.
+                 * */
+                if( intent.resolveActivity( activity!!.packageManager ) == null ){
+                    intent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://www.linkedin.com/profile/view?id=$LINKED_IN_ID")
+                    )
+                }
+
+                activity!!.startActivity( intent )
+            }
+
+
+    /*
+     * Método responsável por conter o algoritmo que invoca
+     * o código de atualização de título da atividade.
+     * */
+    override fun onResume() {
+        super.onResume()
+        (activity as MainActivity)
+            .updateActivityTitle( getString( R.string.about_title ) )
+    }
+}
---
+++ app/src/main/java/thiengo/com/br/thiengo_corretordeimveis/MainActivity.kt
@@ -0,0 +1,114 @@
+package thiengo.com.br.thiengo_corretordeimveis
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+
+class MainActivity :
+    AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer_layout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
+        aboutPage()
+    }
+
+    override fun onBackPressed() {
+        if( drawer_layout.isDrawerOpen(GravityCompat.START) ){
+            drawer_layout.closeDrawer(GravityCompat.START)
+        }
+        else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        /*
+         * Para realizar o contato via WhatsApp assim que houver o
+         * acionamento da opção "Contactar corretor".
+         * */
+        when (item.itemId) {
+            R.id.nav_contact -> {
+                whatsAppHelp()
+            }
+            else -> {
+                aboutPage()
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return false // Para não mudar o item selecionado em menu gaveta
+    }
+
+    /*
+     * Método ouvidor para permitir que o usuário entre em contato
+     * com o WhatsApp correto com apenas um acionamento em tela.
+     * */
+    fun whatsAppHelp(){
+        /* O número abaixo é fictício. */
+        val whatsAppUri = Uri.parse( "smsto:${AboutFragment.WHATS_APP_NUMBER}" )
+        val intent = Intent( Intent.ACTION_SENDTO, whatsAppUri )
+
+        intent.setPackage( "com.whatsapp" )
+
+        /*
+         * Garantindo que a Intent somente será acionada se o
+         * aplicativo WhatsApp estiver presente no aparelho.
+         * */
+        if( intent.resolveActivity( packageManager ) != null ){
+            startActivity( intent )
+        }
+        else{
+            Toast
+                .makeText(
+                    this,
+                    getString(R.string.whatsapp_needed_info),
+                    Toast.LENGTH_SHORT
+                )
+                .show()
+        }
+    }
+
+    /*
+     * Método responsável por invocar a AboutFragment como
+     * o fragmento atual em tela.
+     * */
+    private fun aboutPage(){
+        val transaction = supportFragmentManager.beginTransaction()
+        val fragment = AboutFragment()
+
+        transaction.replace( R.id.fl_frag_container, fragment )
+        transaction.commit()
+    }
+
+    /*
+     * Método responsável por conter o código de atualização
+     * de título da atividade.
+     * */
+    fun updateActivityTitle( title: String ){
+        toolbar.title = title
+    }
+}
